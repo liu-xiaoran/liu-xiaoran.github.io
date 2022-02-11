335bc1ded83663c450b0e3f80859b28644fe1cae
liuhaixiao <mk9540@163.com>
2022-02-11 22:49:10 +0800

    修改名称

--- renamesh.sh
+++ renamesh.sh
@@ -1,6 +1,6 @@
 git filter-branch -f --env-filter "
-GIT_AUTHOR_NAME='liuxiaoran';
+GIT_AUTHOR_NAME='liuhaixiao';
 GIT_AUTHOR_EMAIL='mk9540@163.com';
-GIT_COMMITTER_NAME='liuxiaoran';
+GIT_COMMITTER_NAME='liuhaixiao';
 GIT_COMMITTER_EMAIL='mk9540@163.com'
 " HEAD
